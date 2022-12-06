3dedaf260078124a0ca5e485f8b1bb536be72d1a
TravisHeeter <heetertc@gmail.com>
2022-12-06 08:49:00 -0500

    Fixed indentation; moved event listeners to f
    I changed all the indentation to 2 spaces. I also moved the quick subtract 45 event listeners to a new function - this was previously evaluated before the subtract45 element was added to the page, so it would work on 2nd run, but not the first.

--- killerSudokuHelper.js
+++ killerSudokuHelper.js
@@ -46,27 +46,47 @@ function FortyFiveSet(n){
 // Creates the new FortyFiveSet object based on what's currently in the sum box.
 function appendMultiples(n=0){ 
   let ff = new FortyFiveSet(n)
-
-  $(`.puzzle-page-container`).append(`
-    <div id="fortyFives" style="height: 100%;width: 126px;position: absolute;top: 0;right: 226px;">
-    
- ${ff.html.toString().split(',').join('')}
-        <div style=‘font-weight:bold’>’m’ to analyze sums</div>
-        <div style='border-top:1px solid white; width: 119px; text-align:center; padding:5px 0; margin-top:5px'>
-            Quick Subtract 45
-        </div>
-        <div>
-            <input style='width:64px' id='subtract45' />-45 = 
-            <input style='width:23px' id='less45Result' />
-        </div>
-        <div style='padding:5px 0'>
-            Use p above to add
-        </div>
-        <div>
-            <input style='width:119px' id='addResult' />
-        </div>
+  
+  $(`.puzzle-page-container`)
+    .append(`
+      <div 
+        id="fortyFives" 
+        style="
+          height: 100%;
+          width: 126px;
+          position: absolute;
+          top: 0;
+          right: 226px;
+        "
+      >
+      ${ff.html.toString().split(',').join('')}
+      <div style=‘font-weight:bold’>
+        ’m’ to analyze sums
+      </div>
+      <div 
+        style='
+          border-top:1px solid white; 
+          width: 119px; 
+          text-align:ccenter; 
+          padding:5px 0; 
+          margin-top:5px
+        '
+      >
+        Quick Subtract 45
+      </div>
+      <div>
+        <input style='width:64px' id='subtract45' /> -45 = 
+        <input style='width:23px' id='less45Result' />
+      </div>
+      <div style='padding:5px 0'>
+        Use p above to add
+      </div>
+      <div>
+        <input style='width:119px' id='addResult' />
+      </div>
     </div>
-    `)
+  `)
+  attachEventListeners()
 }
 
 // launches the helper
@@ -74,36 +94,38 @@ $(`#fortyFives`).length ? removeAndAddMultiples() : appendMultiples()
 
 // When 'm' is pressed, calculate with the current sum.
 document.onkeypress = function(e) {
-    e = e || window.event;
-    if(e.which === 109)
-        fillSum()
+  e = e || window.event;
+  if(e.which === 109)
+    fillSum()
 };
 
-$('#subtract45').on('input', function(){
+function attachEventListeners(){
+  $('#subtract45').on('input', function(e){
     let v = this.value
     let result = 0
+    
     // Treat 'p' like '+' - add two numbers before subtracting 45
     if(v.includes('p')){
-        let pi = v.indexOf('p')
-        let first = parseInt(v.substring(0,pi))
-        let last = v.substring(pi+1) !== '' ? parseInt(v.substring(pi+1)) : 0
-        let r = first + last
-        $('#addResult').val(first + ' + ' + last + ' = ' + r)
-        v = r
+      let pi = v.indexOf('p')
+      let first = parseInt(v.substring(0,pi))
+      let last = v.substring(pi+1) !== '' ? parseInt(v.substring(pi+1)) : 0
+      let r = first + last
+      $('#addResult').val(first + ' + ' + last + ' = ' + r)
+      v = r
     } 
     // If input has 3 digits, remove first two
     else {
-    
-        let s = v.toString()
-        let l = s.length
-        if(l > 2){
-            v = parseInt(s[2])
-            $(this).val(v).trigger('change')
-        }
+      let s = v.toString()
+      let l = s.length
+      if(l > 2){
+        v = parseInt(s[2])
+        $(this).val(v).trigger('change')
+      }
     }
     result = 45-v
     $('#less45Result').val(result)
-}).on('click', function(){
+  }).on('click', function(){
     // on click, highlight input
     $(this).select()
-})
+  })
+}
